f5fe64ecf45512a1ab35a3f61cdef9808bc0805a
thisisfaaris <thisisfaaris@gmail.com>
2021-09-11 18:31:38 +0530

    header and log change

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -1,5 +1,5 @@
-import { IoIosArrowDropdown } from 'react-icons/io';
-import styled from 'styled-components';
+import { IoIosArrowDropdown } from "react-icons/io";
+import styled from "styled-components";
 
 export const Container = styled.div`
   display: grid;
@@ -29,7 +29,6 @@ export const Div1 = styled.div`
 export const Div2 = styled.div`
   grid-area: 1 / 2 / 2 / 4;
   display: flex;
-  margin-top: 0.75em;
   justify-content: space-between;
   @media ${(props) => props.theme.breakpoints.sm} {
     grid-area: 2 / 2 / 3 / 5;
@@ -96,8 +95,8 @@ export const NavProductsIcon = styled(IoIosArrowDropdown)`
   display: flex;
   align-self: center;
   transition: 0.3s ease;
-  opacity: ${({ isOpen }) => (isOpen ? '1' : '.75')};
-  transform: ${({ isOpen }) => (isOpen ? 'scaleY(-1)' : 'scaleY(1)')};
+  opacity: ${({ isOpen }) => (isOpen ? "1" : ".75")};
+  transform: ${({ isOpen }) => (isOpen ? "scaleY(-1)" : "scaleY(1)")};
 
   &:hover {
     opacity: 1;
@@ -109,18 +108,42 @@ export const NavProductsIcon = styled(IoIosArrowDropdown)`
   }
 `;
 
-
-// Social Icons 
+// Social Icons
 
 export const SocialIcons = styled.a`
-transition: 0.3s ease;
-color: white;
-border-radius: 50px;
+  transition: 0.3s ease;
+  color: white;
+  border-radius: 50px;
   padding: 8px;
-&:hover {
+  &:hover {
     background-color: #212d45;
     transform: scale(1.2);
     cursor: pointer;
-    
   }
-`
+`;
+
+//Button
+
+export const Button = styled.button`
+  background: #57be6c;
+  color: #ffffff;
+  font-size: 1em;
+  font-weight: 600;
+  padding: 0.5em 2em;
+  border: 1px solid #57be6c;
+  border-radius: 3px;
+  transition: 0.5s;
+  cursor: pointer;
+  &:hover {
+    background: #3c854b;
+    border: 1px solid #3c854b;
+  }
+
+  @media ${(props) => props.theme.breakpoints.md} {
+    font-size: 0.8em;
+  }
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    font-size: 0.8em;
+  }
+`;
